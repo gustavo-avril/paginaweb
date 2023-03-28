786127d1c76dd2b1bd788506b8e9893f37da5195
gustavo-avril <gustavo.tallaferro@avril-assistance.com>
2023-03-28 14:14:41 -0300

    quienes somos

---
+++ js/counter.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 25, 2023 09:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the HTML
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "EXPIRED";
+    document.getElementById("hours").innerHTML = "EXPIRED";
+    document.getElementById("minutes").innerHTML = "EXPIRED";
+    document.getElementById("seconds").innerHTML = "EXPIRED";
+  }
+}, 1000);
