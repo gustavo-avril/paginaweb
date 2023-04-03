ae835f117faee9f241fbc06a342447562471ef82
gustavo-avril <gustavo.tallaferro@avril-assistance.com>
2023-04-03 09:54:05 -0300

    contador de eventos

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 29, 2023 18:00:00").getTime();
+var countDownDate = new Date("Apr 05, 2023 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
